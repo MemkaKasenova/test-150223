066d355fa23a1f410ab305311370274699044985
Memka <mkasenova87@gmail.com>
2023-02-16 15:12:12 +0600

    доп

--- test 150223/Magazine.swift
+++ test 150223/Magazine.swift
@@ -14,34 +14,14 @@ class Magizine {
     init(nameMag: String) {
         self.nameMag = nameMag
     }
-
-    func priceTovar(_ p1: String, _ p2: String, _ p3: String) {
-
-        print("- \(nameMag)-")
-        print("Товары:")
-        print("Наим-е:              Сумма:")
-
-        print("Введите товар")
-
+    
+    func priceTovar(_ p: String, _ p2: String, _ p3: String) {
+        
         for i in tovar {
-            if i.key == p1 {
+            if i.key == p {
                 print("Продукт: \(i.key), цена: \(i.value)")
                 price1 = i.value
             }
-            if i.key == p2 {
-                print("Продукт: \(i.key), цена: \(i.value)")
-                price2 = i.value
-            }
-            if i.key == p3 {
-                print("Продукт: \(i.key), цена: \(i.value)")
-                price3 = i.value
-            }
         }
-
-        print("Итого к оплате:\(price1 + price2 + price3) сом")
-        print("-------------------")
-        print("Cпасибо за покупку!")
     }
 }
-
-
